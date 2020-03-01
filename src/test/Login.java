679eba562050cc36c140ec7961004a4c091f8425
ashaipi <58574961+ashaipi@users.noreply.github.com>
2020-03-01 18:11:59 -0500

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -4,5 +4,7 @@ public class Login {
     public static void main(String[] args) {
         // new code in test package and Bookit project
         System.out.println("user logged in");
+        
+        // From GitHub
     }
 }
