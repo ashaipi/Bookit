f13516ca3dbd68c663f99271e5d8a4c610b814e8
ashaipi <ashaipi@yahoo.com>
2020-03-01 15:52:25 -0500

    add Login.java

---
+++ src/test/Login.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Login {
+    public static void main(String[] args) {
+        // new code in test package and Bookit project
+        System.out.println("user logged in");
+    }
+}
